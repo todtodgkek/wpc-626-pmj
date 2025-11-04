e458eecb9a9afec84ffcd3c20db03482639d9d30
min ju <minju000309@naver.com>
2025-11-04 16:11:01 +0900

    ss

--- 007.JS학습/js/js4-4.array_sort.js
+++ 007.JS학습/js/js4-4.array_sort.js
@@ -303,7 +303,6 @@ const arrString = ["파", "타", "하", "가", "바", "사", "다", "라", "차"
 //   arrNumber.sort((a, b) => (a == b ? 0 : a > b ? -1 : 1))
 // );
 
-
 // [1] 숫자로만된 배열의 정렬 ///////////////////////////
 
 // [1-1] 출력대상 : .showNum
@@ -312,3 +311,143 @@ const showNum = document.querySelector(".showNum");
 // [1-2] 현재 숫자배열 출력하기
 // 배열데이터 : arrNumber
 console.log("숫자배열원본:", arrNumber);
+
+// [1-3] 현재 숫자배열 출력하기 함수 //////
+const showNumFn = (newArray) => {
+  // newArray: 전달된 배열
+  // 배열데이터만큼 숫자 이미지를 만들어서 화면에 출력하기
+  // 순서: 먼저 출력요소에 html넣기 셋팅후
+  // 이퀄 오른쪽에서 배열만큼 map을 돌아서 코드를 생성한다!
+  showNum.innerHTML = newArray
+    .map(
+      (v) => `
+    <img src="./images/num/num_0${v}.png" 
+    alt="숫자이미지">`
+    )
+    .join("");
+}; ////////////////// showNumFn함수 /////////////////
+
+// 기본 숫자배열로 화면출력함수 최초호출하기
+showNumFn(arrNumber);
+
+// [1-4] 숫자정렬 선택박스 변경시 실행함수 /////////
+document.querySelector("#sel").addEventListener("change", function () {
+  // 선택된 선택박스의 value값
+  const selValue = this.value;
+
+  // console.log('선택값:', selValue);
+
+  if (selValue == 1) {
+    // 오름차순 정렬
+    arrNumber.sort((a, b) => (a == b ? 0 : a < b ? -1 : 1));
+  } else if (selValue == 2) {
+    // 내림차순 정렩
+    arrNumber.sort((a, b) => (a == b ? 0 : a > b ? -1 : 1));
+  }
+  showNumFn(arrNumber);
+}); /////////////// 숫자정렬 선택박스 변경시 실행함수 //
+
+// [2] 문자로만된 배열의 정렬 ///////////////////////////
+
+// [2-1] 출력대상 : .showNum2
+const showNum2 = document.querySelector(".showNum2");
+
+// [2-2] 현재 문자배열 출력하기
+// 배열데이터 : arrString
+console.log("문자배열원본:", arrString);
+
+// [2-3] 현재 문자배열 출력하기 함수 //////
+const showNumFn2 = (newArray) => {
+  // newArray: 전달된 배열
+  // 배열데이터만큼 숫자 이미지를 만들어서 화면에 출력하기
+  // 순서: 먼저 출력요소에 html넣기 셋팅후
+  // 이퀄 오른쪽에서 배열만큼 map을 돌아서 코드를 생성한다!
+  showNum2.innerHTML = newArray.map((v) => `<span>${v}</span>`).join("");
+}; ////////////////// showNumFn2함수 /////////////////
+
+// 기본 문자배열로 화면출력함수 최초호출하기
+showNumFn2(arrString);
+
+// [2-4] 문자정렬 선택박스 변경시 실행함수 /////////
+document.querySelector("#sel2").addEventListener("change", function () {
+  // 선택된 선택박스의 value값
+  const selValue = this.value;
+
+  // console.log('선택값:', selValue);
+
+  if (selValue == 1) {
+    // 오름차순 정렬
+    arrString.sort((a, b) => (a == b ? 0 : a < b ? -1 : 1));
+  } else if (selValue == 2) {
+    // 내림차순 정렬
+    arrString.sort((a, b) => (a == b ? 0 : a > b ? -1 : 1));
+  }
+  showNumFn2(arrString);
+}); /////////////// showNumFn2함수 /////////////////
+
+// [3] 객체데이터 배열의 정렬 //////////////
+// [3-1] 객체데이터 배열
+// - 객체구조 :
+// (1) idx - 순번 / (2) tit - 제목 / (3) cont - 내용
+const list1 = [
+  {
+    idx: 8,
+    tit: "나는 구누?",
+    cont: "공동구매) 슬로건 공구 (계좌와 네이버폼)",
+  },
+  {
+    idx: 4,
+    tit: "여기는 어디?",
+    cont: "총공 공지] 오늘부터 일 2회, 총공 진행합니다",
+  },
+  {
+    idx: 1,
+    tit: "나야나",
+    cont: "연합 갈라 서포트 계좌오픈",
+  },
+  {
+    idx: 15,
+    tit: "이제 얼마나 남은거니?",
+    cont: "음악프로그램에 출연 요청글도 써볼까요?",
+  },
+]; /////////////// list1 /////////////
+
+console.log(list1);
+
+// [3-2] 데이터 바인딩하기 : 함수화하여 재사용!
+// 바인딩 출력대상
+const showList3 = myFn.qs(".showList3");
+
+const showList3Fn = (newArray) => {
+  // newArray 데이터 바인딩할 배열
+  showList3.innerHTML = `
+        <table>
+          <thead>
+            <tr>
+              <th>번호</th>
+              <th>제목</th>
+              <th>내용</th>
+            </tr>
+          </thead>
+          <tbody>
+          ${newArray
+            .map(
+              (v) => `            
+                <tr>
+                    <td>${v.idx}</td>
+                    <td>${v.tit}</td>
+                    <td>${v.cont}</td>
+                </tr>
+                `
+            )
+            .join("")}
+            
+            </tbody>
+                </table>
+    
+    `;
+}; //////// showList3Fn 함수 //////////
+
+// 바인딩함수 최초호출!
+showList3Fn(list1);
+console.log("객체배열원본:", list1);
--- 009.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
+++ 009.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
@@ -113,7 +113,7 @@ $room.each((idx, el) => {
 }); /////// each 메서드 /////////////
 
 // 좀비는 모두 숨기기!
-$('.mz').hide();
+$(".mz").hide();
 
 // 2. 버튼 셋팅하기 //////////////////////
 // 대상 : .btns button -> $btns변수
@@ -129,43 +129,177 @@ const actMini = (el, seq, fn) => {
   // seq : 이동할 li방 순번
   // fn : 이동후 실행할 콜백함수
 
+  console.log("이동방순번:", seq);
+
   // 1. 메시지 숨기기
   $msg.fadeOut(300);
 
   // 2. 클릭된 버튼 자신 숨기기
-  $(this).slideUp(300);
+  $(el).slideUp(300);
 
   // 3. 방의 위치 알아내기
   // -> 요소의 위치는 offset().top / offset().left 메서드 사용
   let myRoom = {}; // 위치정보 객체
-  let myTarget = $room.eq(8); // n번방
+  let myTarget = $room.eq(seq); // n번방
   myRoom.top = myTarget.offset().top;
   // 방 중앙에 오도록 방크기의 절반만큼 이동하고 자기크기의
   // 절반만큼 빼줌!
   myRoom.left = myTarget.offset().left + myTarget.width() / 2 - $mi.width() / 2;
-  console.log("8번방위치:", myRoom);
+  console.log("n번방위치:", myRoom);
 
   // 4. 미니언즈 이동하기
-  $mi.animate({
-    top:myRoom.top,
-    left:myRoom.left
-  },800,// 시간
-  'easeOutElastic', // 이징
-  ()=>{ // 이동후 콜백함수
-
-    // 5. 메시지 변경후 나타나기 
-    // -> 방번호와 같은 순번 메시지
-    // -> 도착 1초후 나타나기
-    $msg.html(msgTxt[8]).delay(1000).fadeIn(300);
-
-  });
+  $mi.animate(
+    {
+      top: myRoom.top,
+      left: myRoom.left,
+    },
+    800, // 시간
+    "easeOutElastic", // 이징
+    fn // 이동후 콜백함수
+  );
   // animate({CSS설정},시간,이징,함수);
-
 }; ///////////// actMini 함수 ///////////////
 
 // 4. 버튼 클릭하여 기능수행하기 ////////////////
 // (4-1) "들어가기" 버튼 클릭시
-$btns.first().click(function () {
-  
+$btns
+  .first()
+  .click(function () {
+    // 버튼별 기능구현하기 //////////////
+    let fn = () => {
+      // 이동후 콜백함수
 
-})
+      // [1] 메시지 변경후 나타나기
+      // -> 방번호와 같은 순번 메시지
+      // -> 도착 1초후 나타나기
+      $msg
+        .html(msgTxt[8])
+        .delay(1000)
+        .fadeIn(300, () => {
+          // 콜백함수
+          // [2] 다음버튼 보이기
+          $(this).next().fadeIn(300);
+        });
+    }; // fn 함수 끝 //////////////
+
+    // 3. 미니언즈 공통 기능함수 호출하기
+    actMini(this, 8, fn);
+  }) /////////// click //////////////
+
+  // (4-2) "옆방으로!" 버튼 클릭시
+  .next()
+  .click(function () {
+    // 버튼별 기능구현하기 //////////////
+    let fn = () => {
+      // 이동후 콜백함수
+
+      // [1] 좀비나타나기 : 현재방의 좀비선택
+      $room
+        .eq(9)
+        .find(".mz") // 좀비선택
+        .delay(1000) // 1초후
+        .fadeIn(300, () => {
+          // 좀비등장후 콜백
+          // [2] 메시지 나타나기
+          $msg.html(msgTxt[9]).css({ left: "-100%" }).fadeIn(300);
+
+          // [3] 다음버튼 보이기
+          $(this).next().fadeIn(300);
+        }); /////// fadeIn ////////////
+    }; // fn 함수 끝 //////////////
+
+    // 3. 미니언즈 공통 기능함수 호출하기
+    actMini(this, 9, fn);
+  }) /////////// click ////////////////
+
+  // (4-3) "윗층으로 도망가!" 버튼 클릭시
+  .next()
+  .click(function () {
+    // 버튼별 기능구현하기 //////////////
+    let fn = () => {
+      // 이동후 콜백함수
+      // [1] 첫번째 메시지 나타나기
+      $msg
+        .html(msgTxt[7][0])
+        .delay(500) // 0.5초후
+        .fadeIn(300) // 나타나기
+        .delay(2000) // 2초후
+        .fadeIn(300, () => {
+          // 다시 나타나기
+          // [2] 좀비 나타나기 : 7번방의 좀비
+          $room
+            .eq(7)
+            .find(".mz") // 좀비선택
+            .delay(1000) // 1초후
+            .fadeIn(300, 
+              // function() { // 일반익명함수 this는 좀비!
+              () => { // 일반익명함수 this는 좀비!
+              console.log('this의미:', this);
+              // 좀비등장후 콜백
+              // [3] 다음 메시지 나타나기
+              $msg.html(msgTxt[7][1]);
+              // [4] 다음버튼 보이기
+              $(this).next().fadeIn(300);
+            }); /////// fadeIn ////////////
+        }); ///////// fadeIn ////////////
+    }; // fn 함수 끝 //////////////
+
+    // 3. 미니언즈 공통 기능함수 호출하기
+    actMini(this, 7, fn);
+  }) /////////// click ////////////////
+
+
+  // (4-4) "다시옆방으로!" 버튼 클릭시
+  .next()
+  .click(function () {
+    // 버튼별 기능구현하기 //////////////
+    let fn = () => {
+      // 이동후 콜백함수
+      // [1] 첫번째 메시지 보이기
+      $msg
+        .html(msgTxt[6][0])
+        .delay(500) // 0.5초후
+        .fadeIn(300) // 나타나기
+        .delay(2000) // 2초후
+        .fadeIn(300, () => {
+          // [2] 두번째 메시지 보이기
+          $msg.html(msgTxt[6][1]);
+          // [3] 다음버튼 보이기
+          $(this).next().fadeIn(300);
+        })
+    }; // fn 함수 끝 //////////////
+
+    // 3. 미니언즈 공통 기능함수 호출하기
+    actMini(this, 6, fn);
+  }) /////////// click ////////////////
+
+  // (4-5) "무서우니 윗층으로!" 버튼 클릭시
+  .next()
+  .click(function () {
+    // 버튼별 기능구현하기 //////////////
+    let fn = () => {
+      // 이동후 콜백함수
+      // [1] 여러 메시지를 순차적으로 보이게하기
+      $msg
+      .html(msgTxt[4][0][0])
+        .fadeIn(200)
+        .delay(500)
+        .fadeIn(200,()=>$msg.html(msgTxt[4][0][1]))
+        .delay(500)
+        .fadeIn(200,()=>$msg.html(msgTxt[4][0][2]))
+        .delay(500)
+        .fadeIn(200,()=>$msg.html(msgTxt[4][0][3]))
+        .delay(500)
+        .fadeIn(200,()=>$msg.html(msgTxt[4][0][4]))
+        .delay(500)
+        .fadeIn(200,()=>$msg.html(msgTxt[4][0][5]))
+        .delay(500)
+        .fadeIn(200,()=>$msg.html(msgTxt[4][0][6]))
+        .delay(500)
+        .fadeIn(200,()=>$msg.html(msgTxt[4][0][7]))
+      
+    }; // fn 함수 끝 //////////////
+
+    // 3. 미니언즈 공통 기능함수 호출하기
+    actMini(this, 4, fn);
+  }) /////////// click ////////////////
